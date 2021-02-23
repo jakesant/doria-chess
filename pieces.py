e39136089be0448b1a207311f625a6225c1bf305
jake.sant.18 <jake.sant.18@um.edu.mt>
2021-02-23 11:09:42 +0100

    Added docstring for piece functions. Adding more logic

--- pieces.py
+++ pieces.py
@@ -1,10 +1,15 @@
 class Pawn:
+    """On first move, a pawn can move 2 spaces forward.
+    
+    On subsequent moves, a pawn can move only 1 space forward."""
+
     first_move = True
-    hor_range = 0
-    vert_range = 2
+    can_move_back = False #Pawn can only move forward
 
     def __init__(self):
         self.first_move = True
+        self.hor_range = 0
+        self.vert_range = 2
     
     def move(self):
         if(self.first_move is True):
@@ -13,8 +18,12 @@ class Pawn:
 
 class Rook:
     def __init__(self):
+        self.hor_range = 8
+        self.vert_range = 8
         
 class Knight:
+    """Moves in an 'L'-shaped formation"""
+
     def __init__(self):
 
 class Bishop:
@@ -25,3 +34,4 @@ class Queen:
         
 class King:
     def __init__(self):
+        self.is_checked = False
