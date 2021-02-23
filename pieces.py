823a27ee914f8e762f1348ffd504aa27958e3300
jake.sant.18 <jake.sant.18@um.edu.mt>
2021-02-23 10:40:47 +0100

    Started implementing logic

--- pieces.py
+++ pieces.py
@@ -1,5 +1,15 @@
 class Pawn:
+    first_move = True
+    hor_range = 0
+    vert_range = 2
+
     def __init__(self):
+        self.first_move = True
+    
+    def move(self):
+        if(self.first_move is True):
+            self.first_move = False
+        
 
 class Rook:
     def __init__(self):
