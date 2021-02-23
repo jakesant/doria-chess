f19686071e35bf946a43e9ffe625c0086859912b
jake.sant.18 <jake.sant.18@um.edu.mt>
2021-02-23 09:48:57 +0100

    Empty classes with each chess piece

---
+++ pieces.py
@@ -0,0 +1,17 @@
+class Pawn:
+    def __init__(self):
+
+class Rook:
+    def __init__(self):
+        
+class Knight:
+    def __init__(self):
+
+class Bishop:
+    def __init__(self):
+        
+class Queen:
+    def __init__(self):
+        
+class King:
+    def __init__(self):
