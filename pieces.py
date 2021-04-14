ae6d7e839461543df63cfba1206e06a1a8289692
jake.sant.18 <jake.sant.18@um.edu.mt>
2021-04-14 13:17:04 +0200

    Making pieces subclasses of a new Piece class

--- pieces.py
+++ pieces.py
@@ -1,4 +1,14 @@
-class Pawn:
+class Piece:
+    """Superclass of piece. Other pieces are subclasses
+    
+    inheriting this"""
+
+    def __init__(self):
+        self.first_move = True
+        self.can_move_back = False
+        self.movement_range = [0][0]
+
+class Pawn(Piece):
     """On first move, a pawn can move 2 spaces forward.
     
     On subsequent moves, a pawn can move only 1 space forward."""
@@ -7,6 +17,7 @@ class Pawn:
     can_move_back = False #Pawn can only move forward
 
     def __init__(self):
+        super().__init__()
         self.first_move = True
         self.hor_range = 0
         self.vert_range = 2
@@ -16,26 +27,31 @@ class Pawn:
             self.first_move = False
         
 
-class Rook:
+class Rook(Piece):
     """Moves any space directly horizontally or vertically"""
 
     def __init__(self):
+        super().__init__()
         self.hor_range = 8
         self.vert_range = 8
         
-class Knight:
+class Knight(Piece):
     """Moves in an 'L'-shaped formation"""
 
     def __init__(self):
+        super().__init__()
 
-class Bishop:
+class Bishop(Piece):
     """Moves diagonally"""
 
     def __init__(self):
-        
-class Queen:
+        super().__init__()
+
+class Queen(Piece):
     def __init__(self):
-        
-class King:
+        super().__init__()
+
+class King(Piece):
     def __init__(self):
+        super().__init__()
         self.is_checked = False
